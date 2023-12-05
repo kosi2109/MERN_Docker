b308bea42715711972d54b056f32356e6d6a63bd
viabells-sithuhtet <sithuhtet@i.viabells.com>
2023-12-05 10:16:26 +0630

    add: env and volume mount for dev

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,14 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send(`Express + TypeScript Server ${process.env.Name || "Default"}`);
+});
+
+app.get("/user", (req: Request, res: Response) => {
+  return res.json({
+    name : "Si Thu Htet",
+    age: 24
+  });
 });
 
 app.listen(port, () => {
